587ba72adf4002e2059f231086cfb0c144166a2e
noopuragadkari <noopuragadkari@gmail.com>
2022-05-16 14:23:36 -0700

    Detail view added

--- finchcollector/main_app/urls.py
+++ finchcollector/main_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
      #http://localhost:8000/finches/
     path('finches/', views.finches_index, name='index'),
+
+    path('finches/<int:finch_id>/', views.finches_detail, name='detail'),
 ]
