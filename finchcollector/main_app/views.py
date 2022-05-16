f568b052a04685df62d934d3ace7199f4f406e4b
noopuragadkari <noopuragadkari@gmail.com>
2022-05-16 13:47:31 -0700

    Model added

--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Finch
 import logging 
 logging.basicConfig(level=logging.DEBUG)
 
 from django.db import models
-
-
-class Finch: 
-  def __init__(self, name, description, lifespan):
-    self.name = name
-    self.description = description
-    self.lifespan = lifespan
-
-finches = [
-  Finch('The Spice Finch', 'Is a sparrow-sized estrildid finch native to tropical Asia', 3),
-  Finch('The Zebra Finch', 'The Australian zebra finch or chestnut-eared finch is the most common estrildid finch of Central Australia.', 2),
-  Finch('The Strawberry Finch',' It is found in the open fields and grasslands of tropical Asia and is popular as a cage bird due to the colourful plumage of the males in their breeding season.', 3),
-  Finch('Double-Barred Finch', 'The double-barred finch is an estrildid finch found in dry savannah.', 2)
-]
 
 # Create your views here.
 def home(request):
@@ -39,4 +26,5 @@ def finches_index(request):
    cats index page
     http://localhost:8000/finches
   """
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html',{'finches':finches})
