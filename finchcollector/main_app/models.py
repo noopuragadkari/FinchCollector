fecb23a4744b6467e4fab9b24e83e977de1153f9
noopuragadkari <noopuragadkari@gmail.com>
2022-05-18 14:07:16 -0700

    Feeding model added

--- finchcollector/main_app/models.py
+++ finchcollector/main_app/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.urls import reverse
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 class Finch(models.Model):
     name = models.CharField(max_length=100)
@@ -7,3 +14,17 @@ class Finch(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Feeding(models.Model):
+    date = models.DateField("feeding date")
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
